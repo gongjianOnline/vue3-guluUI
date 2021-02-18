9a50bcc1d14e68506f597ced23f8c13495d0f406
李承斌 <15031617367@163.com>
2021-02-18 14:31:45 +0800

    添加toggleAside

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -5,7 +5,7 @@
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
-    <span class="toggleMenu"></span>
+    <span class="toggleAside"></span>
   </div>
 </template>
 <script lang='ts'>
@@ -41,8 +41,15 @@ export default {
       margin: 0 1em;
     }
   }
-  > .toggleMenu{
-
+  > .toggleAside{
+    display: inline-block;
+    width: 24px;
+    height: 24px;
+    background: red;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
   }
   @media (max-width:500px) {
     > .menu{display: none;}
