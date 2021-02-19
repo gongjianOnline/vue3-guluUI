34c49b92f064993af7c4113c7f4546fe93048836
李承斌 <15031617367@163.com>
2021-02-19 10:10:42 +0800

    可以切换了

--- src/lib/switch.vue
+++ src/lib/switch.vue
@@ -1,11 +1,44 @@
 <template>
-    <div>switch组件</div>
+    <button :class="{checked:x}" @click="toggle">
+        <span></span>
+    </button>
 </template>
 <script lang='ts'>
+import { ref } from 'vue'
 export default {
-    
+    setup(){
+        const x = ref(false);
+        const toggle = ()=>{
+            x.value = !x.value
+        }
+        return {x,toggle}
+    }
 }
 </script>
 <style lang="scss" scoped>
-
+$h:22px;
+$h2:$h - 4px;
+button{
+    height: $h;
+    width: $h * 2;
+    border:none;
+    background: grey;
+    border-radius:$h/2;
+    position: relative;
+}
+span{
+    position: absolute;
+    top:2px;
+    left:2px;
+    height:$h2;
+    width:$h2;
+    background: white;
+    border-radius: $h2 / 2;
+}
+button.checked{
+    background: blue;
+}
+button.checked >span{
+    left: calc(100% - #{$h2} - 2px)
+}
 </style>
