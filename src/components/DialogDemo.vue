0905dc0f12b40a7c2528da1877257692c215d0f7
李承斌 <15031617367@163.com>
2021-02-20 11:06:21 +0800

    dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <h1>示例一</h1>
         <Button @click="toogle">toggle</Button>  
         <Dialog v-model:visible="x" 
                 @update:visible="x = $event"
@@ -15,10 +16,21 @@
             </template>   
         </Dialog>
     </div>
+    <div>
+        <h1>示例二</h1>
+        <Button @click="showDialog">toggle</Button>  
+        <Dialog v-model:visible="x" 
+                @update:visible="x = $event"
+                :closeOnClickOverlay="false"
+                :ok="f1"
+                :cancel="f2">   
+        </Dialog>
+    </div>
 </template>
 <script>
 import Dialog from '../lib/Dialog.vue'
 import Button from '../lib/button.vue'
+import {openDialog} from "../lib/openDialog.ts"
 import { ref } from 'vue'
 export default {
     components:{
@@ -34,7 +46,19 @@ export default {
             return false
         }
         const f2 = ()=>{}
-        return {x,toogle,f1,f2}
+        const showDialog = ()=>{
+            openDialog({
+                title:"标题",
+                content:"hello",
+                ok:()=>{
+                    console.log('ok')
+                },
+                cancel:()=>{
+                    console.log('cencel')
+                }
+            })
+        }
+        return {x,toogle,f1,f2,showDialog}
     }
 
 }
