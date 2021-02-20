5ad24fcbd08d893df7783a78316fc57fd46e5733
李承斌 <15031617367@163.com>
2021-02-20 17:30:01 +0800

    tab未完成

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,20 +1,25 @@
 <template>
   <div>
     <h1>示例一</h1>
-    <Tabs>
-      <Tab title="导航一">内容一</Tab>
-      <Tab title="导航一">内容二</Tab>
+    <Tabs v-model:selected='x'>
+      <Tab title="导航1">内容一</Tab>
+      <Tab title="导航2">内容二</Tab>
     </Tabs>
   </div>
 </template>
 <script>
 import Tabs from "../lib/Tabs.vue";
 import Tab from "../lib/Tab.vue";
+import { ref } from 'vue';
 export default {
   components: {
     Tabs,
     Tab,
   },
+  setup(){
+    const x = ref('导航1')
+    return{x}
+  }
 };
 </script>
 <style lang="scss" scoped>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,10 +1,32 @@
 <template>
-    <div v-for="t in titles" :key="t">{{t}}</div>
-    <component v-for="c in defaults" :key="c" :is="c"/>
+    <div class="gulu-tabs">
+        <div class="gulu-tabs-nav">
+            <div class="gulu-tabs-nav-item"
+                :class="{selected:t === selected}"
+                @click="select(t)"
+                v-for="(t,index) in titles"
+                :key="index">
+                {{t}}
+            </div>
+            <div class="gulu-tabs-nav-indicator"></div>
+        </div>
+        <div class="gulu-tabs-content">
+            <component class="gulu-tabs-content-item"
+                :class="{selected: c.props.title === selected }"
+                v-for="c in defaults"
+                :key="c"
+                :is="c"></component>
+        </div>
+    </div>
 </template>
 <script lang="ts">
 import Tab from "./Tab.vue"
 export default {
+    props:{
+        selected:{
+            type:String
+        }
+    },
     setup(props,context){
         const defaults = context.slots.default();
         defaults.forEach((tag)=>{
@@ -15,10 +37,51 @@ export default {
         const titles = defaults.map((tag)=>{
             return tag.props.title
         })
-        return {defaults,titles}
+        const select = (title: string) => {
+            context.emit('update:selected', title)
+        }
+        return {defaults,titles,select}
     }    
 }
 </script>
 <style lang="scss">
-
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.gulu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    position: relative;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+    &-indicator{
+      position:absolute;
+      height:3px;
+      background: $blue;
+      left:0;
+      bottom:-1px;
+      width: 100px; 
+    }
+  }
+  &-content {
+    padding: 8px 0;
+    &-item{
+        display: none;
+        &.selected{
+            display:block;
+        }
+    }
+  }
+}
 </style>
