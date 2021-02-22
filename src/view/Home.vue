b78399d43d3223b86f8c28d505a3142728f31b5e
李承斌 <15031617367@163.com>
2021-02-22 10:39:48 +0800

    网站装修

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="TopnavAndBanner">
-      <Topnav/>
+      <Topnav />
       <div class="banner">
         <h1>轱辘UI</h1>
         <h2>一个UI框架</h2>
@@ -12,19 +12,31 @@
       </div>
     </div>
     <div class="features">
-      <svg class="icon" aria-hidden="true">
-        <use xlink:href="#icon-Vue"></use>
-      </svg>
-      <svg class="icon" aria-hidden="true">
-        <use xlink:href="#icon-ts"></use>
-      </svg>
-      <svg class="icon" aria-hidden="true">
-        <use xlink:href="#icon-dengpao"></use>
-      </svg>
+      <ul>
+        <li>
+          <svg>
+            <use xlink:href="#icon-Vue"></use>
+          </svg>
+          <h3>基于 Vue 3</h3>
+          <p>骄傲地使用了 Vue 3 Composition API</p>
+        </li>
+        <li>
+          <svg>
+            <use xlink:href="#icon-ts"></use>
+          </svg>
+          <h3>基于 TypeScript</h3>
+          <p>源代码采用 TypeScript 书写（非严格检查）</p>
+        </li>
+        <li>
+          <svg>
+            <use xlink:href="#icon-dengpao"></use>
+          </svg>
+          <h3>代码易读</h3>
+          <p>每个组件的源代码都极其简洁</p>
+        </li>
+      </ul>
     </div>
-    
   </div>
-  
 </template>
 <script lang='ts'>
 import Topnav from "../components/Topnav.vue";
@@ -35,39 +47,75 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-$green:#02bcb0;
-$border-radius:4px;
-$color:#007974;
-.TopnavAndBanner{
-  background: linear-gradient(145deg, rgba(3,255,253,1) 0%, rgba(183,233,230,1) 100%);
+$green: #02bcb0;
+$border-radius: 4px;
+$color: #007974;
+.TopnavAndBanner {
+  background: linear-gradient(
+    145deg,
+    rgba(3, 255, 253, 1) 0%,
+    rgba(183, 233, 230, 1) 100%
+  );
+  clip-path: ellipse(80% 60% at 50% 40%);
 }
-.features{
-  > svg{
-    width: 64px;
-    height: 64px;
+.features {
+  margin: 64px auto;
+  width: 400px;
+  @media (min-width: 800px) {
+    width: 800px;
+  }
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+  >ul {
+    display: flex;
+    flex-wrap: wrap;
+    >li {
+      width: 400px;
+      margin: 16px 0;
+      display: grid;
+      justify-content: start;
+      align-content: space-between;
+      grid-template-areas:
+        "icon title"
+        "icon text";
+      grid-template-columns: 80px auto;
+      grid-template-rows: 1fr auto;
+      >svg {
+        grid-area: icon;
+        width: 64px;
+        height: 64px;
+      }
+      >h3 {
+        grid-area: title;
+        font-size: 28px;
+      }
+      >p {
+        grid-area: text
+      }
+    }
   }
 }
 .banner {
-  color:$color;
+  color: $color;
   padding: 100px 0;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
   // background: lightgreen;
-  > .actions{
+  > .actions {
     padding: 8px 0;
     a {
-      margin:0 8px;
+      margin: 0 8px;
       background: $green;
-      color:#fff;
+      color: #fff;
       display: inline-block;
       border-radius: $border-radius;
-      padding: 8px 24px;  
-      &:hover{
+      padding: 8px 24px;
+      &:hover {
         text-decoration: none;
       }
-       
     }
   }
 }
