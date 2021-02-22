0f3ed59d6417474719aeb97d5014c5fdc6d3a2b2
李承斌 <15031617367@163.com>
2021-02-22 10:23:41 +0800

    进入阿里图标

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="topnav">
-    <div class="logo" @click="toggleMenu">LOGO</div>
+    <div class="logo" @click="toggleMenu">
+      <svg class="icon">
+        <use xlink:href="#icon-wuxian"></use>
+      </svg>
+    </div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
@@ -21,8 +25,11 @@ export default {
 }
 </script>
 <style lang='scss' scoped>
+$color:#007974;
+
 .topnav {
   // background: pink;
+  color:$color;
   display: flex;
   position: fixed;
   top: 0;
@@ -35,6 +42,10 @@ export default {
   > .logo {
     max-width: 6em;
     margin-right: auto;
+    > svg{
+      width: 32px;
+      height: 32px;
+    }
   }
   > .menu {
     display: flex;
--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,15 +1,30 @@
 <template>
-  <div class="TopnavAndBanner">
-    <Topnav/>
-    <div class="banner">
-      <h1>轱辘UI</h1>
-      <h2>一个UI框架</h2>
-      <p class="actions">
-        <a href="https:/github.com">GitHub</a>
-        <router-link to="/doc">开始</router-link>
-      </p>
+  <div>
+    <div class="TopnavAndBanner">
+      <Topnav/>
+      <div class="banner">
+        <h1>轱辘UI</h1>
+        <h2>一个UI框架</h2>
+        <p class="actions">
+          <a href="https:/github.com">GitHub</a>
+          <router-link to="/doc">开始</router-link>
+        </p>
+      </div>
     </div>
+    <div class="features">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-Vue"></use>
+      </svg>
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-ts"></use>
+      </svg>
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-dengpao"></use>
+      </svg>
+    </div>
+    
   </div>
+  
 </template>
 <script lang='ts'>
 import Topnav from "../components/Topnav.vue";
@@ -20,10 +35,20 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+$green:#02bcb0;
+$border-radius:4px;
+$color:#007974;
 .TopnavAndBanner{
   background: linear-gradient(145deg, rgba(3,255,253,1) 0%, rgba(183,233,230,1) 100%);
 }
+.features{
+  > svg{
+    width: 64px;
+    height: 64px;
+  }
+}
 .banner {
+  color:$color;
   padding: 100px 0;
   display: flex;
   justify-content: center;
@@ -34,13 +59,15 @@ export default {
     padding: 8px 0;
     a {
       margin:0 8px;
-      background: #fff;
+      background: $green;
+      color:#fff;
       display: inline-block;
-      $h: 28px;
-      height: $h;
-      line-height: $h;
-      border-radius: $h/2;
-      padding: 0 8px;
+      border-radius: $border-radius;
+      padding: 8px 24px;  
+      &:hover{
+        text-decoration: none;
+      }
+       
     }
   }
 }
